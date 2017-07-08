b32a0dfe7d4c53e30ef0320fa1c68f711e4522da
hutou <1125281337@qq.com>
2017-07-09 01:26:48 +0800

    简单修改

--- eurekaCustomer/src/main/java/me/helllp/demo/eurekaCustomer/service/FeignService.java
+++ eurekaCustomer/src/main/java/me/helllp/demo/eurekaCustomer/service/FeignService.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-	@FeignClient(value="EUREKACLIENT", configuration=FeignConfiguration.class)
-	public interface FeignService {
-		@RequestMapping(value = "/hello",method = RequestMethod.GET)
-	    String sayHiUseFeign(@RequestParam(value = "name") String name);
-	}
+@FeignClient(value="EUREKACLIENT", configuration=FeignConfiguration.class)
+public interface FeignService {
+	@RequestMapping(value = "/hello",method = RequestMethod.GET)
+    String sayHiUseFeign(@RequestParam(value = "name") String name);
+}
